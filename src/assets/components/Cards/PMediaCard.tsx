59f6b99d3bb2a4736204f98ad1566aa8cb216ed1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-27 16:29:21 -0500

    Add ID generation and hover state to PMediaCard
    Introduced unique ID generation for PMediaCard components when ID is not provided via props. Also added hover state management and necessary imports to enhance component interactivity.

--- src/assets/components/Cards/PMediaCard.tsx
+++ src/assets/components/Cards/PMediaCard.tsx
@@ -1,5 +1,9 @@
+import {cn} from "@nextui-org/react";
+import {useEffect, useState} from "react";
+
 interface PMediaCardProps
 {
+    id?: string;
     title?: string;
     subtitle?: string;
     year?: number;
@@ -18,8 +22,34 @@ interface PMediaCardProps
 
 export default function PMediaCard(props: PMediaCardProps)
 {
+    const [id, setId] = useState(props.id ?? "");
+    const [isHovering, setIsHovering] = useState(false);
+    useEffect(() =>
+    {
+        if (!props.id)
+        {
+            setId(`media-card-${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`);
+        }
+    }, []);
+
+    useEffect(() =>
+    {
+        if(id)
+        {
+
+        }
+    }, [id]);
+
     return (
-        <>
-        </>
+        <div
+            id={id}
+            className={
+                cn(
+                    "w-48 aspect-[9/14] rounded-lg overflow-hidden shadow-lg shrink-0",
+                    "bg-white/10"
+                )
+            }
+        >
+        </div>
     );
 }
