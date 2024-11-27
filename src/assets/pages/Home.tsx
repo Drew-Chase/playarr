580f20da046f8f9cd3eac712adec7751723dedce
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-27 15:17:53 -0500

    Set page title for Home component
    Added a function call to set the page title to "Home" when the Home component is rendered. This improves the user experience by appropriately labeling the browser tab.

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,4 +1,7 @@
+import {setTitle} from "../../main.tsx";
+
 export default function Home() {
+    setTitle("Home")
     return (
         <>
             <h1 className="text-3xl font-bold underline">Home Page</h1>
