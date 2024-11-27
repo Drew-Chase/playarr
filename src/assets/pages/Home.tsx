fc31f23090c677a0d24da34388fb8a9ed3f69689
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-27 16:29:04 -0500

    Add PCarousel and PMediaCard components to Home page
    Enhanced the Home page by integrating the PCarousel and PMediaCard components. The carousel displays a list of 100 media cards, and includes a title and subtitle with an alert action for demonstration purposes. This improves the visual appeal and functionality of the Home page.

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,10 +1,24 @@
 import {setTitle} from "../../main.tsx";
+import PCarousel from "../components/PCarousel.tsx";
+import PMediaCard from "../components/Cards/PMediaCard.tsx";
 
-export default function Home() {
-    setTitle("Home")
+export default function Home()
+{
+    setTitle("Home");
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Home Page</h1>
-        </>
+        <div className={"flex flex-col items-center justify-center w-full gap-4"}>
+            <PCarousel
+                title={"Popular"}
+                subtitle={"Popular media"}
+                action={() =>
+                {
+                    alert("hi");
+                }}
+            >
+                {Array.from({length: 100}).map((_) => (
+                    <PMediaCard/>
+                ))}
+            </PCarousel>
+        </div>
     );
 }
