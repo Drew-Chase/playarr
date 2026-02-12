fd2fb0b72bdd149c1552f1e9afb0291f4bfc9c0b
Drew Chase (Home) <dcmanproductions@gmail.com>
2026-02-12 17:21:07 -0500

    Import `ProxyViteOptions` in `lib.rs` to prepare for Vite integration.

--- src-actix/lib.rs
+++ src-actix/lib.rs
@@ -2,6 +2,7 @@ use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use serde_json::json;
 use log::*;
 use anyhow::Result;
+use vite_actix::proxy_vite_options::ProxyViteOptions;
 use crate::asset_endpoint::AssetsAppConfig;
 use vite_actix::start_vite_server;
 
